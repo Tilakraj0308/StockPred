91e329149c19ed12f73c1a11c6967079aa8b97d5
Tilakraj0308 <tilakraj7050@gmail.com>
2023-04-02 19:19:04 +0530

    Updated model

--- model.py
+++ model.py
@@ -6,7 +6,8 @@ def get_model(last_days = 30) -> tf.keras.models.Sequential:
     
     model = tf.keras.models.Sequential()
     model.add(tf.keras.layers.LSTM(128, return_sequences=True, input_shape= (last_days-1, 1)))
-    model.add(tf.keras.layers.LSTM(64, return_sequences=False))
+    model.add(tf.keras.layers.LSTM(64, return_sequences=True))
+    model.add(tf.keras.layers.LSTM(32, return_sequences=False))
     model.add(tf.keras.layers.Dense(25))
     model.add(tf.keras.layers.Dense(1))
 
